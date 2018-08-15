82abe291cf88a882e6439d4d7e1de5b21d56fb21
phongdk92 <phongdk92@gmail.com>
2018-08-15 17:01:41 +0700

    prepare full data

--- main.py
+++ main.py
@@ -29,12 +29,13 @@ model_name= 'model_' + datetime.strftime(datetime.now(), "%Y_%m_%d_%H_%M")
 #print model_name
 #sys.exit()
 
-def convert_to_wav(folder):
-    for folder in sorted(os.listdir(folder)):
+def convert_to_wav(path_to_folder):
+    for folder in sorted(os.listdir(path_to_folder)):
         if "pkl" in folder:
             continue
         print folder
-        curDir = os.path.join(os.getcwd(), path, folder)
+        curDir = os.path.join(os.getcwd(), path_to_folder, folder)
+        print curDir
         for (i,filename) in enumerate(os.listdir(curDir)):
             print i
             path_to_file = os.path.join(curDir,filename)
@@ -45,11 +46,10 @@ def convert_to_wav(folder):
                 new_file = path_to_file.replace(extension, '.wav')
                 command = "ffmpeg -i {} {}".format(path_to_file, new_file)
                 os.system(command)
-                #print commands.getoutput(command)
                 os.system('rm {}'.format(path_to_file))     #remove old file not in *.wav format
     
-def get_mfcc_feature(path_to_folder):
-    train_pkl = os.path.join(path_to_folder,'0_female_central.pkl')
+def get_mfcc_feature(path_to_folder='.'):
+    train_pkl = os.path.join(path_to_folder,'train.pkl')
     if os.path.isfile(train_pkl):
         [training_set, maxlen, max_cepstrum, min_cepstrum] = cPickle.load(open(train_pkl, 'rb'))
         return training_set, maxlen, max_cepstrum, min_cepstrum
@@ -60,17 +60,8 @@ def get_mfcc_feature(path_to_folder):
     maxlen = 0
     list_folders = sorted(os.listdir(path_to_folder))
     list_folders = [folder for folder in list_folders if "pkl" not in folder and 'zip' not in folder]
-    #print list_folders
-    #sys.exit()
     for (order, folder) in enumerate(list_folders):
-        #if "pkl" in folder or 'zip' in folder:
-        #    continue
-        if order <= 1:
-            continue
-#        print folder
-        folder_pkl = os.path.join(path, str(order) + '_' + folder + '.pkl')
-        print folder_pkl, 
-        #continue
+        print folder, 
         gender = 0 if 'female' in folder else 1
         if 'north' in folder:
             accent = 0
@@ -81,32 +72,15 @@ def get_mfcc_feature(path_to_folder):
         print accent, gender
         curDir = os.path.join(os.getcwd(), path, folder)
         for (i,filename) in enumerate(os.listdir(curDir)):
-            print i
             path_to_file = os.path.join(curDir,filename)
-            #print path_to_file
-#            if not filename.endswith('wav'):
-#                #convert to wav
-#                command = "ffmpeg -i {}".format(path_to_file)
-#                #print commands.getoutput(command)
-#                extension = os.path.splitext(filename)[-1]
-#                new_file = path_to_file.replace(extension, '.wav')
-#                command = "ffmpeg -i {} {}".format(path_to_file, new_file)
-#                os.system(command)
-#                #command = "ffmpeg -i {}".format(new_file)
-#                #print commands.getoutput(command)
-#                
-#                (rate,sig) = wav.read(new_file)
-#                os.system('rm {}'.format(new_file))     #remove new *.wav file
-#            else:
             (rate,sig) = wav.read(path_to_file)
-                #print sig
-                #plt.plot(mfcc_feat)
-                #fig, ax = plt.subplots()
-                #mfcc_data= np.swapaxes(mfcc_feat, 0 ,1)
-                #cax = plt.imshow(mfcc_data, interpolation='nearest', cmap=cm.coolwarm, origin='lower')
-                #plt.show()                
-                #sys.exit()
-            mfcc_feat = mfcc(sig,rate, nfft = 1024)
+            mfcc_feat = mfcc(sig,rate, nfft = 2048)
+            #plt.plot(mfcc_feat)
+            #fig, ax = plt.subplots()
+            #mfcc_data= np.swapaxes(mfcc_feat, 0 ,1)
+            #cax = plt.imshow(mfcc_data, interpolation='nearest', cmap=cm.coolwarm, origin='lower')
+            #plt.show() 
+            
             maxlen = max(maxlen, mfcc_feat.shape[0])        #find max time-length
             
             max_current_mfcc = np.max(mfcc_feat, axis = 0)
@@ -122,25 +96,35 @@ def get_mfcc_feature(path_to_folder):
             sample['gender'][gender] = 1
             sample['accent'][accent] = 1
             training_set.append(sample)
-            #break
         print len(training_set)
-        cPickle.dump([training_set, maxlen, max_cepstrum, min_cepstrum], open(folder_pkl,'wb'), -1)
-    #del training_set            
-        #break
-    #cPickle.dump([training_set, maxlen, max_cepstrum, min_cepstrum], open(train_pkl,'wb'), -1)
-    #print len(training_set)
+    cPickle.dump([training_set, maxlen, max_cepstrum, min_cepstrum], open(train_pkl,'wb'), -1)
     return training_set, maxlen, max_cepstrum, min_cepstrum       
         
 def normalize_data(dataset, min_cepstrum, max_cepstrum):
     diff = max_cepstrum - min_cepstrum
-    for data in dataset:
-        data['feature'] = (data['feature'] - min_cepstrum) / diff    #(x-min)/(max-min)
+    if isinstance(dataset, dict):
+        for data in dataset:
+            data['feature'] = (data['feature'] - min_cepstrum) / diff    #(x-min)/(max-min)
+    else:
+        for data in dataset:
+            data = (data - min_cepstrum) / diff    #(x-min)/(max-min)
     return dataset
 
-def padding_data(dataset, maxlen):
-    for data in dataset:
-        data['feature'] = np.pad(data['feature'], [(0,maxlen - len(data['feature'])),(0,0)], mode='constant', constant_values=0)
-    
+def get_same_length_data(dataset, maxlen):
+    if isinstance(dataset, dict):
+        for data in dataset:
+            if data['feature'].shape[0] < maxlen:           #padding data
+                data['feature'] = np.pad(data['feature'], [(0,maxlen - len(data['feature'])),(0,0)], mode='constant', constant_values=0)
+            else:
+                data['feature'] = data['feature'][:maxlen]          #clip data
+    else:
+        for data in dataset:
+            if data.shape[0] < maxlen:           #padding data
+                data = np.pad(data, [(0,maxlen - len(data)),(0,0)], mode='constant', constant_values=0)
+            else:
+                data = data[:maxlen]          #clip data
+    return dataset
+ 
 def split_data(training_set):
     train, test = train_test_split(training_set, test_size = 0.3, random_state=42)
     X_train = np.array([element['feature'] for element in train])
@@ -157,16 +141,21 @@ def split_data(training_set):
 
 def model(training_set, maxlen, use_dropout = True):
     X_train, Y_train_gender, Y_train_accent, X_test, Y_test_gender, Y_test_accent =  split_data(training_set)
-    
+    print 'Split data'
+    print 'number of training set', len(X_train)
+    print 'number of validation set', len(X_test)
     inputs = Input(shape=(maxlen,13,))
 #    lstm1 = LSTM(16, return_sequences=True)(inputs)
 #    lstm1_flatten = Flatten()(lstm1)
 #    dense1 = Dense(32, activation='relu')(lstm1_flatten)
-    lstm1 = LSTM(16)(inputs)
-    dense1 = Dense(8, activation='relu')(lstm1)
-    output_gender = Dense(2, activation='softmax')(dense1)
-    output_accent = Dense(3, activation='softmax')(dense1)
+    lstm1 = LSTM(128)(inputs)
+    dense1_gender = Dense(64, activation='relu')(lstm1)
+    dense2_gender = Dense(32, activation='relu')(dense1_gender)
+    output_gender = Dense(2, activation='softmax')(dense2_gender)
     
+    dense1_accent = Dense(64, activation='relu')(lstm1)
+    dense2_accent = Dense(32, activation='relu')(dense1_accent)
+    output_accent = Dense(3, activation='softmax')(dense2_accent)
     model = Model(input=inputs, output=[output_gender, output_accent])
     model.compile(loss = 'categorical_crossentropy', optimizer='adam', metrics = ['accuracy'])
     
@@ -183,7 +172,7 @@ def model(training_set, maxlen, use_dropout = True):
     
     #model.fit(X_train, Y_train_gender, batch_size=10, verbose=1, epochs=3)
     model.fit(X_train, [Y_train_gender, Y_train_accent], validation_data=(X_test, [Y_test_gender, Y_test_accent]), \
-              batch_size=128, verbose=1, epochs=1)
+              batch_size=32, verbose=1)
 
     # serialize model to JSON
     model_json = model.to_json()
@@ -193,14 +182,48 @@ def model(training_set, maxlen, use_dropout = True):
     model.save_weights(model_name + ".h5")
     print("Saved model to disk")
     
+def get_new_max_length(dataset):
+    l = [data['feature'].shape[0] for data in dataset]
+    iqr = np.subtract(*np.percentile(l, [75, 25]))
+    maxlen = int(np.percentile(l, 75) + 1.5 * iqr)       #set new max length for audio since the longest is over 200K
+    return maxlen
+
 if __name__ == "__main__":
     #convert_to_wav(path)
     training_set, maxlen, max_cepstrum, min_cepstrum = get_mfcc_feature(path)
-    print maxlen
-    print max_cepstrum
-    print min_cepstrum
+    print 'number of training samples', len(training_set)
+#    print maxlen
+#    print max_cepstrum
+#    print min_cepstrum
     training_set = normalize_data(training_set, min_cepstrum=min_cepstrum, max_cepstrum=max_cepstrum)
-    padding_data(training_set, maxlen=maxlen)
+    maxlen = get_new_max_length(training_set)
+    print 'new max length for padding', maxlen
+    training_set = get_same_length_data(training_set, maxlen=maxlen)
+    
+#    for data in training_set:
+#        assert data['feature'].shape[0] == maxlen
     model(training_set, maxlen)
     #print training_set[0:2]['gender']
-    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- test.py
+++ test.py
@@ -18,11 +18,17 @@ import keras as K
 from keras.models import Sequential, Model, model_from_json
 from keras.layers import Dense, Activation, LSTM, Dropout, Input, Flatten, InputLayer 
 from keras.preprocessing.sequence import pad_sequences
+import cPickle
 
-from main import convert_to_wav, normalize_data, padding_data
+from main import normalize_data, get_same_length_data, get_new_max_length
 model_name = ''
 
-def get_mfcc_feature(model, path):
+def get_mfcc_feature(path):
+    print 'extract feature of test set'
+    test_pkl = 'test.pkl'
+    if os.path.isfile(test_pkl):
+        [test_set, list_filenames] = cPickle.load(open(test_pkl, 'rb'))
+        return test_set, list_filenames
     test_set = []
     list_filenames = sorted(os.listdir(path))
     for filename in list_filenames:
@@ -30,7 +36,6 @@ def get_mfcc_feature(model, path):
         if not filename.endswith('wav'):
             #convert to wav
             command = "ffmpeg -i {}".format(path_to_file)
-            #print commands.getoutput(command)
             extension = os.path.splitext(filename)[-1]
             new_file = path_to_file.replace(extension, '.wav')
             command = "ffmpeg -i {} {}".format(path_to_file, new_file)
@@ -43,8 +48,9 @@ def get_mfcc_feature(model, path):
         else:
             (rate,sig) = wav.read(path_to_file)
              
-        mfcc_feat = mfcc(sig,rate, nfft = 1024)
+        mfcc_feat = mfcc(sig,rate, nfft = 2048)
         test_set.append(mfcc_feat)
+    cPickle.dump([test_set, list_filenames], open(test_pkl, 'wb'), -1)
     return test_set, list_filenames
 
 def predict_labels(list_filenames, test_set):
@@ -59,15 +65,33 @@ def predict_labels(list_filenames, test_set):
     
     labels = loaded_model.predict(test_set)
     
-    
+def convert_to_wav_test(folder):
+    for (i,filename) in enumerate(os.listdir(folder)):
+        print i
+        path_to_file = os.path.join(folder,filename)
+        if not filename.endswith('wav'):
+            #convert to wav
+            command = "ffmpeg -i {}".format(path_to_file)
+            extension = os.path.splitext(filename)[-1]
+            new_file = path_to_file.replace(extension, '.wav')
+            command = "ffmpeg -i {} {}".format(path_to_file, new_file)
+            os.system(command)
+            os.system('rm {}'.format(path_to_file))     #remove old file not in *.wav format    
+            
 if __name__ == "__main__":
     path_to_public_test = sys.argv[1]
     if ".zip" in path_to_public_test:
-        os.command("unzip {}".format(path_to_public_test))
-        path_to_public_test.replace(".zip", " ")
-    convert_to_wav(path_to_public_test)
-    #maxlen, max_cepstrum, min_cepstrum must come from training_set
-    test_set, maxlen, max_cepstrum, min_cepstrum = get_mfcc_feature()
-    normalize_data(test_set, min_cepstrum=min_cepstrum, max_cepstrum=max_cepstrum)
-    padding_data(test_set)
-    predict_labels(test_set)
+        #os.system("unzip {}".format(path_to_public_test))
+        path_to_public_test = path_to_public_test.replace(".zip", "")
+    #convert_to_wav_test(path_to_public_test)
+    train_pkl = 'train/train.pkl'
+    [training_set, maxlen, max_cepstrum, min_cepstrum] = cPickle.load(open(train_pkl, 'rb'))
+    maxlen = get_new_max_length(training_set)
+    print maxlen
+    
+    test_set, list_filenames = get_mfcc_feature(path_to_public_test)
+    print len(test_set)
+    test_set = normalize_data(test_set, min_cepstrum=min_cepstrum, max_cepstrum=max_cepstrum)
+    test_set = get_same_length_data(test_set, maxlen)
+    
+#    predict_labels(test_set)
